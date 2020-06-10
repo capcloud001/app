c9d6f7ca1ab41c88352b44630e56fd71cf0ef64b
mioru <namiomiru@gmail.com>
2020-06-10 11:03:44 +0900

    1.0.1

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -3,12 +3,7 @@
     <v-container class="fill-height" fluid>
       <v-row align="center" justify="center">
         <v-col class="shrink">
-          <v-tooltip right>
-            <template v-slot:activator="{ on }">
-              <v-btn :href="source" icon large target="_blank" v-on="on"></v-btn>
-            </template>
-            <span>Source</span>
-          </v-tooltip>
+          <span>hello</span>
         </v-col>
       </v-row>
     </v-container>
